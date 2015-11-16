7717edd41c722a9d4a576232edfdbeefbacd0f93
Blablux <hetrube@gmail.com>
2015-11-17 00:24:20 +0100

    adding a working parser

--- README.md
+++ README.md
@@ -5,3 +5,31 @@ remove the notification bar and the virtual buttons.
 
 The script doesn't do much right now, as it is more a sandbox for (re)learning
 Python programming.
+
+## Required modules
+
++ PIL
++ sys
++ os.path
++ argparse
+
+## Usage
+
+__Usage:__
+
+    crop.py [-h] [-v] [-u U] [-l L] image
+
+__Positional arguments:__
+
+    image
+
+__Optional arguments:__
+
+    -h, --help     show this help message and exit  
+    -v, --version  show program's version number and exit  
+    -u U           height of the notification area  
+    -l L           height of the virtual buttons area
+
+## Side Note
+
+Faustus is minor NPC from the video game _Legacy of Kain_.
--- crop.py
+++ crop.py
@@ -5,34 +5,25 @@
 # Required modules: Image (for obvious reasons), sys for arguments parsing
 # and os for filename manipulation.
 from PIL import Image
-import sys, os.path, argparse
+import os.path, argparse
+
+parser = argparse.ArgumentParser(version='0.1')
+parser.add_argument('image')
+parser.add_argument('-u', default=75, help='height of the notification area')
+parser.add_argument('-l', default=144, help='height of the virtual buttons area')
+results = parser.parse_args()
 
 # Check if an image (jpg or png for the sake of simplicity) is provided
-if os.path.isfile(sys.argv[-1]) is False:
-    print 'No file provided'
-    exit()
-    # TODO: Do not exit > help
-name, ext = os.path.splitext(sys.argv[-1])
+if os.path.isfile(results.image) is False:
+    parser.error("The file %s does not exist!" % results.image)
+name, ext = os.path.splitext(results.image)
 if ext != '.png' and ext != '.jpg':
-    print 'File provided is not an image'
-    exit()
-
-# Parse other arguments
-if len(sys.argv) > 2:
-    # DEBUG:
-    print 'Found ' + str(len(sys.argv) - 2) + ' parameters.'
-    param_list = list(sys.argv[1:-1])
-    print str(param_list)
-else:
-    print 'Found no parameters'
-
-# HINT: Default margin values.
-upper_margin = 75
-lower_margin = im.size[1] - 144
+    parser.error("The file %s is not an image!" % results.image)
 
 # Cropping image
-im = Image.open(sys.argv[-1])
+im = Image.open(results.image)
+lower_margin = im.size[1] - results.l
 # HINT: crop(left,up,right,down)
-#region = im.crop( (0,upper_margin,im.size[0],lower_margin) )
+region = im.crop( (0,results.u,im.size[0],lower_margin) )
 newfilename = name + '.cropped' + ext
-#region.save(newfilename)
+region.save(newfilename)
