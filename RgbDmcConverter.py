a9bc048fc2fee44d1a08ff1f9b50f7aa9447114d
Blablux <hetrube@gmail.com>
2016-02-14 23:42:37 +0100

    add option to save table as a text file

--- RgbDmcConverter.py
+++ RgbDmcConverter.py
@@ -12,33 +12,43 @@ class Dmc:
 
     def __init__(self):
         # DOC: URL of the site to fetch
-        self.url = ""
         self.table = {}
 
-    def OpenResource(self, resource):
+    def FetchOnline(self):
         """Gets the remote page and load it in BeautifulSoup"""
-        res = requests.get(resource)
+        res = requests.get('')
         res.raise_for_status()
         self.content = bs4.BeautifulSoup(res.text)
-
-    def FetchColors(self):
-        """Extracts the colors from the content and saves them in an
-        attribute"""
         for row in self.content.select('table.tableborder tr'):
             print (row)
             try:
                 cell = row.select('td')
-                self.table[cell[0].get_text()] = {'name': cell[1].get_text(),
-                                                  'R': cell[2].get_text(),
-                                                  'G': cell[2].get_text(),
-                                                  'B': cell[3].get_text(),
-                                                  'hex': cell[4].get_text()}
+                self.CreateDict(cell)
             except IndexError:
                 pass
 
+    def CreateDict(self, cell):
+        self.table[cell[0].get_text()] = {'name': cell[1].get_text(),
+                                          'R': cell[2].get_text(),
+                                          'G': cell[2].get_text(),
+                                          'B': cell[3].get_text(),
+                                          'hex': cell[4].get_text()}
+
+    def SaveLocal(self, location):
+        self.FetchOnline()
+        try:
+            save = open(location, 'rw')
+            save.write(self.table)
+            save.close()
+        except OSError as e:
+            if isinstance(e, FileNotFoundError):
+                pass
+            else:
+                return False
+
     def Initiate(self):
         """Launches the full script"""
-        self.OpenResource(self.url)
+        self.FetchOnline()
 
 
 if __name__ == '__main__':
