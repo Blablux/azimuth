0d7a3bc603f42e3b130e5288aed1f6a62142412d
Blablux <hetrube@gmail.com>
2016-03-13 22:14:39 +0100

    First fully working code

--- RgbDmcConverter.py
+++ RgbDmcConverter.py
@@ -3,17 +3,18 @@ import requests
 import bs4
 from PIL import Image
 import os.path
-# DEBUG:
-import pprint
+from colormath.color_objects import LabColor, sRGBColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
 
 
 class Dmc:
-    """Class class aimed at converting images (mostly pixel
+    """Class aimed at converting images (mostly pixel
     art) to DMC colors for cross stitching."""
 
     def __init__(self):
-        # DOC: URL of the site to fetch
         self.table = {}
+        self.depth = 128
 
     def Normalize(self, p):
         """Normalize Unix paths"""
@@ -24,11 +25,10 @@ class Dmc:
 
     def FetchOnline(self):
         """Gets the remote page and load it in BeautifulSoup."""
-        res = requests.get()
+        res = requests.get('')  # CONFIGURE: Add the URL to fetch
         res.raise_for_status()
         self.content = bs4.BeautifulSoup(res.text)
         for row in self.content.select('table.tableborder tr'):
-            # DEBUG: print (row)
             try:
                 cell = row.select('td')
                 self.CreateDict(cell)
@@ -36,12 +36,12 @@ class Dmc:
                 pass
 
     def CreateDict(self, cell):
-        """Subtask to create the supported colors table."""
+        """Subtask to create the supported colors palette."""
         self.table[cell[0].get_text()] = {'name': cell[1].get_text(),
                                           'R': cell[2].get_text(),
-                                          'G': cell[2].get_text(),
-                                          'B': cell[3].get_text(),
-                                          'hex': cell[4].get_text()}
+                                          'G': cell[3].get_text(),
+                                          'B': cell[4].get_text(),
+                                          'hex': cell[5].get_text()}
 
     def SaveLocal(self, location):
         """Save color palette to a file for further use"""
@@ -62,15 +62,48 @@ class Dmc:
         """List all colors from an image"""
         try:
             img = Image.open(self.Normalize(location), 'r')
-            self.colors = img.getcolors(256)
+            self.colors = img.getcolors(self.depth)
+            while self.colors is None:
+                self.depth *= 2
+                self.colors = img.getcolors(self.depth)
+
         except OSError as e:
             print ('Error opening the file ', location)
             print (e)
 
+    def FindColorDelta(self, color):
+        closestColor = 255
+        n = 0
+        for key, values in self.table.items():
+            orgColor = sRGBColor(color[0], color[1], color[2], True)
+            dmcColor = sRGBColor(int(values['R']),
+                                 int(values['G']),
+                                 int(values['B']),
+                                 True)
+            initLab = convert_color(orgColor, LabColor)
+            dmcLab = convert_color(dmcColor, LabColor)
+            delta = delta_e_cie2000(initLab, dmcLab)
+            if delta < closestColor:
+                closestColor = delta
+                closestValue = values['name']
+                closestRGB = (int(values['R']),
+                              int(values['G']),
+                              int(values['B']))
+                n = 0
+            elif delta == closestColor:
+                # TODO: Handling when we have two or more identical delta
+                n += 1
+        return closestValue, closestRGB
+
     def Initiate(self):
         """Launches the full script"""
-        self.GetImgColors()
-        pprint.pprint(self.colors)
+        self.FetchOnline()
+        self.GetImgColors('')  # CONFIGURE: Add image to parse
+        print ('Color depth is ' + str(self.depth))
+        print ('found ' + str(len(self.colors)) + ' colors')
+        for key, value in self.colors:
+            color, rgb = self.FindColorDelta(value)
+            print('color ' + str(value) + ' matches ' + color + ' ' + str(rgb))
 
 
 if __name__ == '__main__':
