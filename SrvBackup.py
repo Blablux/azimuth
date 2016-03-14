2dece405d5defe56c0bcf711464fd4b2fb707011
Blablux <hetrube@gmail.com>
2016-03-14 23:48:38 +0100

    Expanded usage

--- SrvBackup.py
+++ SrvBackup.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python
 import os
 import time
+from sh import rsync
 
 
 class SrvBackup:
     """Class for maintaing a complete server backup"""
 
     def __init__(self, path):
-        self.backupdir = path
+        self.hostname = os.uname()[1]
+        self.backupdir = self.Normalize(os.path.join(path, self.hostname))
+        if not os.path.exists(self.backupdir):
+            try:
+                os.makedirs(self.backupdir)
+            except:
+                raise
 
-    def DbDump(user, password, database, host="localhost"):
+    def Normalize(p):
+        """Normalize Unix paths"""
+        if str(p[:1]) == '~':
+            return os.path.expanduser(p)
+        else:
+            return os.path.abspath(p)
+
+    def DbDump(self, user, password, database, host="localhost"):
         filestamp = time.strftime('%Y-%m-%d-%I:%M')
         os.popen("mysqldump -u %s -p%s -h %s -e --opt -c %s | gzip -c > %s.gz"
-                 % (user, password, host, database, database + "_" +
-                    filestamp))
-        print "dump file in " + database + "_" + filestamp + ".gz --"
+                 % (user, password, host, database,
+                    os.path.join(self.backupdir, database + "_" + filestamp)
+                    )
+                 )
+        print "dumped file in " + database + "_" + filestamp + ".gz"
+
+    def Rsync(self, directories):
+        """Syncing directories (supplied as a list) to the backupdir"""
+        for directory in directories:
+            if not os.path.exists(self.Normalize(directory)):
+                print()
+            else:
+                rsync("-av", self.Normalize(directory), self.backupdir)
+
+    def ReadCfg(self, cfgfile):
+        with open(self.Normalize(cfgfile), 'r') as config:
+            return eval(config.read())
+
 
 if __name__ == "__main__":
-    process = SrvBackup('/')
+    process = SrvBackup('/nfs/backup')
     process.DbDump()
